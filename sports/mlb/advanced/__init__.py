8e7899368e11cf2f3d3d1c1461647e0e15f239fc
LukeCampbell3 <161851764+LukeCampbell3@users.noreply.github.com>
2026-09-05 20:03:12 -0400

    Add sequential PA model package

---
+++ sports/mlb/advanced/__init__.py
@@ -0,0 +1,5 @@
+"""Advanced MLB hitter/pitcher process modeling and sequential PA simulation."""
+
+from .sequential_pa_model import MODEL_VERSION, simulate_hitter_market
+
+__all__ = ["MODEL_VERSION", "simulate_hitter_market"]
