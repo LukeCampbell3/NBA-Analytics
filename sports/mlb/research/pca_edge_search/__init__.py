f885ea8fbde46c03f947a063351535cd6a2ad102
LukeCampbell3 <161851764+LukeCampbell3@users.noreply.github.com>
2026-09-16 15:39:24 -0400

    Expose PCA A* residual search research API

---
+++ sports/mlb/research/pca_edge_search/__init__.py
@@ -0,0 +1,15 @@
+from .pca_edge_search import (
+    EdgeSearchResult,
+    HoldoutReport,
+    LocalEdgeEstimate,
+    PCAConfig,
+    PCAStrategyEdgeSearch,
+)
+
+__all__ = [
+    "EdgeSearchResult",
+    "HoldoutReport",
+    "LocalEdgeEstimate",
+    "PCAConfig",
+    "PCAStrategyEdgeSearch",
+]
